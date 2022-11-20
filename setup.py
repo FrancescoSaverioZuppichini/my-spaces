097675e9f392adf8f901fa3911935f55d0452ebe
FrancescoSaverioZuppichini <francesco.zuppichini@gmail.com>
2022-11-20 21:52:27 +0100

    minor changes

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name="my-spaces",
     version="0.0.1",
-    packages=find_packages(include=["src", "src.*"]),
+    packages=find_packages(include=["my_spaces", "my_spaces.*"]),
     install_requires=["jinja2", "typer[all]", "docker"],
     entry_points={
-        "console_scripts": ["my-spaces=src.main:main"],
+        "console_scripts": ["my-spaces=my_spaces.main:main"],
     },
 )
