2dc8007f3dfaa1e4caea9f7fd649bc4e8dade18f
FrancescoSaverioZuppichini <francesco.zuppichini@gmail.com>
2022-11-21 09:52:14 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="my-spaces",
+    description="Self Host Hugging Face Spaces",
     version="0.0.1",
     packages=find_packages(include=["my_spaces", "my_spaces.*"]),
     install_requires=["jinja2", "typer[all]", "docker"],
