293de87dd5e707d331d82d7da837fc60980bee0f
FrancescoSaverioZuppichini <francesco.zuppichini@gmail.com>
2022-11-22 10:01:34 +0100

    added MANIFEST with templates

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     description="Self Host Hugging Face Spaces",
     version="0.0.1",
     packages=find_packages(include=["my_spaces", "my_spaces.*"]),
+    include_package_data=True,
     install_requires=["jinja2", "typer[all]", "docker"],
     entry_points={
         "console_scripts": ["my-spaces=my_spaces.main:main"],
